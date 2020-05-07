cafbb2b198d8ae3b77caf5d5fd8a6d53ab540f40
Wesley Hui <wesleyhui21@gmail.com>
2020-05-06 18:50:12 -0700

    Added Script

--- js/lightbox2/scripts.js
+++ js/lightbox2/scripts.js
@@ -1,4 +1,17 @@
 
 $(document).ready( function(){
     let images = document.getElementsByTagName('a');
+    $('.search').on('keyup', function(){
+        let search = $('.search').val().toLowerCase();
+
+        for (var i =0; i< images.length; i++) {
+            let searchVal = images[i].getAttribute('data-title');
+        
+            if (searchVal.toLowerCase().indexOf(search) > -1){
+                images[i].style.display ="";
+            }else {
+                images[i].style.display = "none";
+            }
+        }
+    });
 });
